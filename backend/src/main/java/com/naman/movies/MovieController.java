720f61181f0120e7da502f6c27fab5b488bdf989
Naman16rajani <naman16rajani@gmail.com>
2023-05-09 16:40:49 +0530

    v0.1.0

---
+++ backend/src/main/java/com/naman/movies/MovieController.java
@@ -0,0 +1,30 @@
+package com.naman.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping(value = "/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movies>> allMovies() {
+        return new ResponseEntity<List<Movies>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movies>> getMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movies>>(movieService.getMovie(imdbId), HttpStatus.OK);
+
+    }
+}
---
+++ backend/src/main/java/com/naman/movies/ReviewController.java
@@ -0,0 +1,23 @@
+package com.naman.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping(value = "/api/v1/reviews",method = RequestMethod.POST)
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+}
